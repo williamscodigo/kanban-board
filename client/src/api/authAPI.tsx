b1a7244cf9ac79baf223f2bc3000c025a6de98b5
williamscodigo <willliamscodigo@gmail.com>
2024-12-04 14:05:03 -0500

    initial commit

---
+++ client/src/api/authAPI.tsx
@@ -0,0 +1,31 @@
+import { UserLogin } from "../interfaces/UserLogin";
+
+const login = async (userInfo: UserLogin) => {
+  // TODO: make a POST request to the login route
+  try {
+    console.log('Userinfo front-end-b4sending: ', userInfo);
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    const data = await response.json();
+    console.log('front-end-response-from-backend: ', data);
+
+    if (!response.ok) {
+      throw new Error('User information not retrieved, check network tab!');
+    }
+
+    return data;
+  } catch (err) {
+    console.log('Error from user login: ', err);
+    return Promise.reject('Could not fetch user info');
+  }
+}
+
+
+
+export { login };
---
+++ client/src/utils/auth.ts
@@ -0,0 +1,40 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    // TODO: return the decoded token
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      // Check if the token has an expiration time and if it’s expired
+      return decoded.exp ? decoded.exp * 1000 < Date.now() : false;
+    } catch (error) {
+      // If there’s an error decoding, treat the token as expired
+      return true;
+    }
+  }
+
+  getToken(): string {
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
+  }
+
+  login(idToken: string) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
